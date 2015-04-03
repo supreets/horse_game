49674751640a988dc2bec3102d035fff08168bed
ssupreet <stonesupreet@gmail.com>
2015-04-03 21:58:51 +0000

    changed create

--- app/controllers/horseinput_controller.rb
+++ app/controllers/horseinput_controller.rb
@@ -1,25 +1,30 @@
 class HorseinputController < ApplicationController
+    
     def new
     end
     
     def create
-        bundles = params[:bundles].to_i
-        distance = params[:distance].to_i
-        bundlesperkm = params[:bundlesperkm].to_i
-        bundlesperride = params[:bundlesperride].to_i
-
-        d = 0
-        while d < distance
-            l = (((bundles/bundlesperride)*(bundlesperkm*2))-1)
-            distanceleft = distance - d
-            if bundles % bundlesperride < 2*bundlesperkm
-                bundles -= bundles % bundlesperride
-            end
-        kilometerspossible = bundlesperride/l
-        d1 = distanceleft < kilometerspossible ? distanceleft : kilometerspossible
-        d += d1
-        bundles -= (d1 * l)
-        end
-        @result = bundles
+    bundles = params[:bundles].to_i
+    distance = params[:distance].to_i
+    bundlesperkm = params[:bundlesperkm].to_i
+    bundlesperride = params[:bundlesperride].to_i
+    
+    d = 0
+    while d < distance
+     if bundles <= 0
+       break;
+     end
+     l = (((bundles/bundlesperride)*2 -1) * bundlesperkm)
+     distanceleft = distance - d
+     if bundles % bundlesperride <= (2*bundlesperkm)
+       bundles -= (bundles % bundlesperride)
+     end
+     kilometerspossible = bundlesperride/l
+     d1 = distanceleft < kilometerspossible ? distanceleft : kilometerspossible
+     d += d1
+     bundles -= (d1 * l)
+    end
+   
+     @result = bundles>0 ? bundles : "bundles exhausted before completing distance"
     end
 end
